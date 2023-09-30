9c3a5054eca76ecd1f1574bc0b7f66678661512f
ChrisW919 <93105879+ChrisW919@users.noreply.github.com>
2023-10-01 05:01:32 +0800

    Time: 3 ms (78.2%), Space: 54.9 MB (15.91%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int s_p=k;
+        int num_len=nums.length;
+        double total=nums[0];
+        for(int i=1;i<k;i++){
+            total+=nums[i];
+        }
+        double max_val=total;
+        for(int j=k;j<num_len;j++){
+            total-=nums[j-k];
+            total+=nums[j];
+            if(total>max_val){
+                max_val=total;
+            }
+        }
+        double factor = 1e5;
+        double result = Math.round((max_val/k) * factor) / factor;
+        return result;
+    }
+}
